4051e2fb750062a79b2fc754d4c6f005c4b9e5c6
Shan060603 <shan.silveo@gmail.com>
2026-02-16 22:03:24 +0800

    feat: Add Kitchen Order Ticket and related documents for restaurant management

---
+++ pos_ff/farmfresh_pos/doctype/restaurant_table/test_restaurant_table.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, SMBITS and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRestaurantTable(FrappeTestCase):
+	pass
